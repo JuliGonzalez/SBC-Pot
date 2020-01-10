64b23e0d48d5179da9372c3b3061645bde217661
Julian Gonzalez <julian.gonzalez@the-cocktail.com>
2020-01-10 14:27:56 +0100

    Added the extract and upload processes

--- src-python/upload_data_BBDD/read_data_from_TB.py
+++ src-python/upload_data_BBDD/read_data_from_TB.py
@@ -1,2 +1,22 @@
 import requests
 
+
+def read_data_from_thingsboard():
+    base = "https://demo.thingsboard.io/api/plugins/telemetry/DEVICE/"
+    access_token = "bd2a9500-1d38-11ea-b102-b79db24c38c3"
+    endpoint = "/values/attributes"
+    url = base + access_token + endpoint
+
+    headers = {
+        'x-authorization': "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJmamdhcmNpYS5hbHZhcmV6QGhvdG1haWwuY29tIiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJ1c2VySWQiOiJkNjA2YWU1MC1lYWQwLTExZTktYmM3Mi1lOWQyMzA2NTUzOTYiLCJmaXJzdE5hbWUiOiJGcmFuY2lzY28gSmF2aWVyIiwibGFzdE5hbWUiOiJHYXJjaWEgQWx2YXJleiIsImVuYWJsZWQiOnRydWUsInByaXZhY3lQb2xpY3lBY2NlcHRlZCI6dHJ1ZSwiaXNQdWJsaWMiOmZhbHNlLCJ0ZW5hbnRJZCI6ImQ1ODA4ZTYwLWVhZDAtMTFlOS1iYzcyLWU5ZDIzMDY1NTM5NiIsImN1c3RvbWVySWQiOiIxMzgxNDAwMC0xZGQyLTExYjItODA4MC04MDgwODA4MDgwODAiLCJpc3MiOiJ0aGluZ3Nib2FyZC5pbyIsImlhdCI6MTU3ODY1NDMyOCwiZXhwIjoxNTgwNDU0MzI4fQ.kTEpGzZbAMWk3YR3ItAEg8i6vIwajJi0tPCDmrH5SvMdO_1G3NUOhTiFpc_dsQtPhnR7OiIhdq2td-Q6-HuxPg",
+        'content-type': "application/json"
+        }
+
+    r = requests.get(url=url, headers=headers)
+    print(r.status_code)
+    print(r.content)
+    return r.content
+
+
+if __name__ == "__main__":
+    read_data_from_thingsboard()
---
+++ src-python/upload_data_BBDD/upload_data_to_mysql.py
@@ -0,0 +1,40 @@
+from mysql.connector import MySQLConnection, Error
+from python_mysql_dbconfig import read_db_config
+
+
+def insert_values(read_date, peso, humedad_suelo_INT, humedad_suelo_EXT, humedad_aire, co2, luminosidad, temperatura,
+                  agua_detectada):
+    query = "INSERT INTO esp32.esp32_thingsboard(read_date, peso, humedad_suelo_INT, humedad_suelo_EXT, humedad_aire, co2, luminosidad, temperatura, agua_detectada) " \
+            "VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    args = (
+        read_date, peso, humedad_suelo_INT, humedad_suelo_EXT, humedad_aire, co2, luminosidad, temperatura,
+        agua_detectada)
+
+    try:
+        db_config = read_db_config()
+        conn = MySQLConnection(**db_config)
+
+        cursor = conn.cursor()
+        cursor.execute(query, args)
+
+        if cursor.lastrowid:
+            print('last insert id', cursor.lastrowid)
+        else:
+            print('last insert id not found')
+
+        conn.commit()
+    except Error as error:
+        print("error")
+        print(error)
+
+    finally:
+        cursor.close()
+        conn.close()
+
+
+def main():
+    insert_values('2020-01-10-8:54', 29, 29, 29, 29, 29, 29, 29, 29)
+
+
+if __name__ == '__main__':
+    main()
