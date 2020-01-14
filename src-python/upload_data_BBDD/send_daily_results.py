500ce3f574924e97a419ab1d76ae3c2fa94f3bc6
Julian Gonzalez <julian.gonzalez@the-cocktail.com>
2020-01-14 12:25:32 +0100

    Added scripts to upload files to Storage and modified requirements.txt

---
+++ src-python/upload_data_BBDD/send_daily_results.py
@@ -0,0 +1,58 @@
+import datetime
+import pandas as pd
+
+from mysql.connector import MySQLConnection, Error
+from python_mysql_dbconfig import read_db_config
+from upload_file_to_storage import StorageRepository
+
+
+def get_results_from_mysql():
+    date_today = datetime.date.today()
+    date_tomorrow = date_today + datetime.timedelta(days=1)
+    print("Lanzando query entre los dias {0} y {1}".format(str(date_today), str(date_tomorrow)))
+    query = "SELECT * FROM esp32.esp32_thingsboard  WHERE read_date > %s AND read_date < %s"
+    args = (date_today, date_tomorrow)
+
+    try:
+        db_config = read_db_config()
+        conn = MySQLConnection(**db_config)
+
+        cursor = conn.cursor()
+        cursor.execute(query, args)
+        records = cursor.fetchall()
+        print(type(records))
+        print(records[0][1])
+        print("Total number of rows is: ", cursor.rowcount)
+        conn.commit()
+    except Error as error:
+        print("error")
+        print(error)
+        return "error in select statement"
+
+    finally:
+        cursor.close()
+        conn.close()
+    return records
+
+
+def transform_to_dataframe(results):
+    columns = ["id", "read_date", "peso", "humedad_suelo_INT", "humedad_suelo_EXT", "humedad_aire", "co2",
+               "luminosidad", "temperatura", "agua_detectada", "rele"]
+    df = pd.DataFrame(columns=columns)
+
+    for row in results:
+        df = df.append({"id": row[0], "read_date": row[1], "peso": row[2], "humedad_suelo_INT": row[3],
+                        "humedad_suelo_EXT": row[4], "humedad_aire": row[5], "co2": row[6], "luminosidad": row[7],
+                        "temperatura": row[8], "agua_detectada": row[9], "rele": row[10]}, ignore_index=True)
+    df.to_csv("results_{}.csv".format(str(datetime.date.today())), sep=';')
+
+
+def main():
+    storage = StorageRepository()
+    results = get_results_from_mysql()
+    transform_to_dataframe(results)
+    storage.load()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src-python/upload_data_BBDD/upload_file_to_storage.py
@@ -0,0 +1,31 @@
+from google.cloud import storage
+import datetime
+import os
+
+
+class StorageRepository:
+    """
+    Clase que realiza la carga del dataframe a un bucket de Storage
+    """
+    _storageRepo = None
+    _file_path = None
+
+    def __init__(self, file_path='results_{}.csv'.format(str(datetime.date.today()))):
+        self._storageRepo = storage.Client.from_service_account_json(
+            '../../storage_SA.json')
+        self._file_path = file_path
+
+    def load(self):
+        """
+        Metodo que carga en CoudStorage los datos de un csv indicado en la creacion de la clase.
+        """
+        bucket = self._storageRepo.get_bucket("resultados_diarios")
+        blob_name = self._file_path
+        blob = bucket.blob(blob_name)
+        blob.upload_from_filename(blob_name)
+        os.remove(self._file_path)
+
+
+if __name__ == '__main__':
+    storage = StorageRepository()
+    storage.load()
